0b28647525f480e6f72bdfed6d207979bb644e7c
Filip <131429926+Filip-Ku@users.noreply.github.com>
2023-06-02 19:44:37 +0200

    Add files via upload

--- src/components/addPost.jsx
+++ src/components/addPost.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AddPost = () => {
-    return <div className="container">
-        <div className="alert alert-dark" role="alert">
-            <h4 className="alert-heading">Dodaj nowy post:</h4>
-            <input type="text" maxLength="25" placeholder="Wprowadz tytul"></input>
-            <input type="text" maxLength="25" placeholder="Link obrazka"></input>
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [text, setText] = useState("");
 
+        const handleSubmit = () => {
+            const postObject = {
+                title: title,
+                text: text,
+                image: image
+            };
+            sendDataToServer(postObject);
+        };
+
+   const sendDataToServer = async (postData) => {
+     try {
+         fetch("http://localhost:3001/api/posts", {
+         method: "POST",
+         headers: {
+           "Content-Type": "application/json"
+         },
+         body: JSON.stringify(postData)
+         });
+            alert("Udało się wstawić nowego posta!")
+        } catch (error) {
+             alert("Coś poszło nie tak jak powinno :C");
+       }
+
+  }
+
+
+    return (
+        <div className="container">
+            <div className="alert alert-dark" role="alert">
+                <h4 className="alert-heading">Dodaj nowy post:</h4>
+                <div id="title">
+                    <input type="text" maxLength="25" placeholder="Wprowadz tytul" value={title} onChange={event => setTitle(event.target.value)}/>
+                </div>
+                <br/>
+                <div id="image">
+                    <input type="text" placeholder="Link obrazka" value={image} onChange={event => setImage(event.target.value)}/>
+                </div>
+                <br/>
+                <div id="text">
+                    <textarea cols="50" rows="7" placeholder="Opis postu" value={text} onChange={(event) => setText(event.target.value)}/>
+                </div>
+                <br/>
+                <button className="btn btn-danger" onClick={handleSubmit}> Wyślij </button>
+            </div>
         </div>
-    </div>;
+    );
 };
 
 export default AddPost;
